a8cfc00744e024893184b09d4b81e2809167acfc
mjaniko <mjaniko@gmail.com>
2026-02-26 17:09:05 +0400

    feat: initial crackode setup with SSG and organized src structure

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,7 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import { ViteReactSSG } from 'vite-react-ssg'
 import App from './App.jsx'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+export const createRoot = ViteReactSSG(
+  { routes: [{ path: '/', element: <App /> }] }
 )
